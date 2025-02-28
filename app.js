7e5d269ed2a7d86187e55365ae73d624a1db3980
shubhankar kumar <sharmashubhankar61@gmail.com>
2025-02-28 16:29:04 +0530

    signup and login page added

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const session=require("express-session");
-const flash=require("connect-flash");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
 
 
-
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+// Routers
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const user = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() => {
@@ -31,14 +35,14 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const sessionOptions={
-    secret:"nirvana",
+const sessionOptions = {
+    secret: "nirvana",
     resave: false,
-    saveUninitialized:true,
-    cookie:{
-        expires:Date.now()+7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
 
@@ -51,9 +55,21 @@ app.get("/", async (req, res, next) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use((req,res,next)=>{
-    res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     next();
 });
 
@@ -61,9 +77,10 @@ app.use((req,res,next)=>{
 
 
 
-
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+// Router use
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
+app.use("/",user);
 
 
 
